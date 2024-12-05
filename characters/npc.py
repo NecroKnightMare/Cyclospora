17508ba460441a072b255a55d068fc4d1e8bd623
SudoSavvy <donovanjheape@gmail.com>
2024-12-05 12:55:44 -0600

    Fixed ninja code using club

--- characters/npc.py
+++ characters/npc.py
@@ -75,9 +75,9 @@ class NPC(Creature):
             """
             Ninja have a chance to throw Shurikens for ranged damage.
             """
-            if random.random() < 0.5:  # 50% chance to throw a rock
-                super().attack(target, Weapon("Shuriken", "ranged", 6, range=5, accuracy=4))
-                print(f"{self.name} throws a rock at {target.name}!")
+            if random.random() < 0.5:  # 50% chance to shoot a bow
+                super().attack(target, Weapon("Bow", "ranged", 6, range=5, accuracy=4))
+                print(f"{self.name} shoots their Bow at {target.name}!")
             else:  # Otherwise, use Tanto for melee attack
                 super().attack(target, Weapon("Tanto", "melee", 8))
 
